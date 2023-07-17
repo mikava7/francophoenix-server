3f3d972c8995bb31c51ff9b88880a5efad61e8e9
Irakli Mikava <i.mikava365@gmail.com>
2023-07-17 16:21:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { json } from "express";
+import connectToDB from "./connect.js";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import courseRouter from "./routes/courseRoutes.js";
+const app = express();
+const PORT = 5500 || process.env.PORT;
+app.use(cors());
+app.use(cookieParser());
+
+app.use(courseRouter);
+
+const start = async () => {
+  try {
+    await connectToDB(process.env.MONGODB_URI);
+
+    app.get("/", (req, res) => {
+      res.send("Hello");
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
+  }
+};
+
+start();
